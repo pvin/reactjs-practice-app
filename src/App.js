1d4b24018b25d3458bcc6f6eab30c589051f8719
praaveen <praaveenranganathan@gmail.com>
2022-01-09 18:27:44 +0530

    fragments 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState, Fragment} from 'react';
 import AddUser from './components/Users/AddUser'
 import UsersList from './components/Users/UsersList'
 
@@ -16,10 +16,12 @@ function App() {
   }
   
   return (
-    <>
+    // <Fragment>
+    <React.Fragment>
       <AddUser onAddUser={addUserList}></AddUser>
       <UsersList users={userList}></UsersList>
-    </>
+    {/* </Fragment> */}
+    </React.Fragment>
   );
 }
 
